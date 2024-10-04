f354db8d3690943863a14dc2cb1e0533a0ddc7c7
Favio <favioramosd@gmail.com>
2024-10-03 19:15:57 -0500

    "agregue un grupo de productos mas vendidos"

---
+++ src/componentes/Grupo/Grupo.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "../Grupo/grupo.css";
+
+export const Grupo = () => {
+    return(
+        <div className="contenedor">
+        <section className="mas-vendido">
+          <h2>Lo más vendido</h2>
+          <div className="productosgeneral">
+            <div className="productos">
+              <img src="" alt="" />
+            </div>
+            <div className="productos">
+            <img src="" alt="" />
+            </div>
+            <div className="productos">
+            <img src="" alt="" />
+            </div>
+            <div className="productos">
+            <img src="" alt="" />
+            </div>
+            <div className="productos">
+            <img src="" alt="" />
+            </div>
+            <div className="productos">
+            <img src="" alt="" />
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+}
+
+export default Grupo;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import { Navbar } from "../../componentes/Navbar/Navbar";
 import { Banner } from "../../componentes/Banner/Banner";
+import { Grupo }  from "../../componentes/Grupo/Grupo";
+
 export const Home = () => {
   return (
     <>
       <Navbar />
       <Banner />
+      <Grupo />
     </>
   )
 };
