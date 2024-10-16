20fdde9b1b226cbc614286d91959a5aa42eda95d
Jerucho <amadorjeremias2006@gmail.com>
2024-10-16 09:52:29 -0500

    correccion de estilos en componente Producto

--- src/router.jsx
+++ src/router.jsx
@@ -2,14 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 import { Productos } from "./pages/Productos/Productos";
 import { Promociones } from "./pages/Promociones/Promociones";
-import { Producto } from "./pages/Producto/Producto";
-
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/productos" element={<Productos />} />
-      <Route path="/productos/:id_producto" element={<Producto />} />
       <Route path="/promociones" element={<Promociones />} />
     </Routes>
   );
