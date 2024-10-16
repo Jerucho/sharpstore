6ece92be235d9e00b35537459d6099b2699ea8bf
Favio Ramos <I202402947@cibertec.edu.pe>
2024-10-16 12:22:40 -0500

    Pagina Promociones arreglada

--- src/componentes/PromoComp/CompoPromo.jsx
+++ src/componentes/PromoComp/CompoPromo.jsx
@@ -2,74 +2,174 @@ import React from "react";
 import "./promoff.css";
 
 const CompoPromo = () => {
-  const images = [
-    {
-      src: "https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/2a9bff8cb30341c0847bac8200bc5fb3_9366/buzo-adicolor-sst.jpg",
-      category: "Promos",
-    },
-    {
-      src: "https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/2a9bff8cb30341c0847bac8200bc5fb3_9366/buzo-adicolor-sst.jpg",
-      category: "Promos",
-    },
-    {
-      src: "https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/2a9bff8cb30341c0847bac8200bc5fb3_9366/buzo-adicolor-sst.jpg",
-      category: "Verano 2025",
-    },
-    {
-      src: "https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/2a9bff8cb30341c0847bac8200bc5fb3_9366/buzo-adicolor-sst.jpg",
-      category: "Verano 2025",
-    },
-    {
-      src: "https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/2a9bff8cb30341c0847bac8200bc5fb3_9366/buzo-adicolor-sst.jpg",
-      category: "80% OFF",
-    },
-    {
-      src: "https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/2a9bff8cb30341c0847bac8200bc5fb3_9366/buzo-adicolor-sst.jpg",
-      category: "80% OFF",
-    },
-  ];
 
   return (
-    <div className="container">
-      <section className="section">
-        <h2>
-          Promosf <span className="red-dot">•</span>
-        </h2>
-        <div className="grid">
-          {images.slice(0, 2).map((image, index) => (
-            <div key={index} className="card">
-              <img src={image.src} alt="Promo" />
+  <>
+  <h1>PROMOS</h1>
+  <section class="mejoresf2p">
+      <div class="carta">
+        <div class="carta-interior">
+          <div class="carta-frente">
+            <div class="tarjeta-juego">
+              <img src="https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/falabellaPE/127176581_1/w=1500,h=1500,fit=pad" alt="" />
+              <div class="juego-descripcion">
+                <span class="juego-titulo">Ropa deportiva</span>
+                <span>S/13,50</span>
+              </div>
             </div>
-          ))}
+          </div>
+        </div>
+      </div>
+      <div class="carta">
+        <div class="carta-interior">
+          <div class="carta-frente">
+            <div class="tarjeta-juego">
+              <img src="https://elcomercio.pe/resizer/qUfVEK1vb1rVG6UAdErd29Cua_U=/980x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/SOXCRHR35RGAXBLDABFRJF2DC4.png" alt="" />
+              <div class="juego-descripcion">
+                <span class="juego-titulo">Ropa Invierno</span>
+                <span>S/31,50</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carta">
+        <div class="carta-interior">
+          <div class="carta-frente">
+            <div class="tarjeta-juego">
+              <img src="https://d3fvqmu2193zmz.cloudfront.net/items_2/uid_commerces.1/uid_items_2.FDLIQ91NB0P1/1500x1500/661968676ECDC-Polo-Futbol-Hombre-Tiro24-Jsy-Aeroready.webp" alt="" />
+              <div class="juego-descripcion">
+                <span class="juego-titulo">Polos deportivos</span>
+                <span>S/34,50</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carta">
+        <div class="carta-interior">
+          <div class="carta-frente">
+            <div class="tarjeta-juego">
+              <img src="https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/falabellaPE/20240417_1/w=1500,h=1500,fit=pad" alt="" />
+              <div class="juego-descripcion">
+                <span class="juego-titulo">-- Conjuntos  --</span>
+                <span>S/40,50</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <h1>Verano 2025</h1>
+    <section class="mejoresf2p">
+        <div class="carta">
+          <div class="carta-interior">
+            <div class="carta-frente">
+              <div class="tarjeta-juego">
+                <img src="https://img.kwcdn.com/product/fancy/5037b024-f1af-41e8-a24f-693f58880f9b.jpg?imageMogr2/auto-orient%7CimageView2/2/w/800/q/70/format/webp" alt="" />
+                <div class="juego-descripcion">
+                  <span class="juego-titulo">-- Sandalias --</span>
+                  <span>S/13,50</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="carta">
+          <div class="carta-interior">
+            <div class="carta-frente">
+              <div class="tarjeta-juego">
+                <img src="https://thebox.com.pe/cdn/shop/files/DK-HT-00019-CML-1_2_1024x1024@2x.jpg?v=1719268655" alt="" />
+                <div class="juego-descripcion">
+                  <span class="juego-titulo">-- Gorras --</span>
+                  <span>S/31,50</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="carta">
+          <div class="carta-interior">
+            <div class="carta-frente">
+              <div class="tarjeta-juego">
+                <img src="https://www.marjoya.com/blog/wp-content/uploads/2016/07/accesorios-verano.jpg" alt="" />
+                <div class="juego-descripcion">
+                  <span class="juego-titulo">-- Pulseras --</span>
+                  <span>S/40,50</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="carta">
+          <div class="carta-interior">
+            <div class="carta-frente">
+              <div class="tarjeta-juego">
+                <img src="https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/falabellaPE/122739510_01/w=800,h=800,fit=pad" alt="" />
+                <div class="juego-descripcion">
+                  <span class="juego-titulo">-- Lentes --</span>
+                  <span>S/28,50</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
-
-      <section className="section">
-        <h2>
-          Verano 2025 <span className="red-dot">•</span>
-        </h2>
-        <div className="grid">
-          {images.slice(2, 6).map((image, index) => (
-            <div key={index} className="card">
-              <img src={image.src} alt="Verano 2025" />
+      <h1>80% OFF </h1>
+      <section class="mejoresf2p">
+        <div class="carta">
+          <div class="carta-interior">
+            <div class="carta-frente">
+              <div class="tarjeta-juego">
+                <img src="https://m.media-amazon.com/images/I/614qHDhUs-L._AC_SL1049_.jpg" alt="" />
+                <div class="juego-descripcion">
+                  <span class="juego-titulo">Ropa de Niños</span>
+                  <span>S/13,50</span>
+                </div>
+              </div>
             </div>
-          ))}
+          </div>
         </div>
-      </section>
-
-      <section className="section">
-        <h2>
-          80% OFF <span className="red-dot">•</span>
-        </h2>
-        <div className="grid">
-          {images.slice(6, 10).map((image, index) => (
-            <div key={index} className="card">
-              <img src={image.src} alt="80% OFF" />
+        <div class="carta">
+          <div class="carta-interior">
+            <div class="carta-frente">
+              <div class="tarjeta-juego">
+                <img src="https://hmperu.vtexassets.com/arquivos/ids/4713452/Polo-de-algodon---Negro---H-M-PE.jpg?v=638612122879930000" alt="" />
+                <div class="juego-descripcion">
+                  <span class="juego-titulo">Polos básicos</span>
+                  <span>S/21,50</span>
+                </div>
+              </div>
             </div>
-          ))}
+          </div>
         </div>
-      </section>
-    </div>
+        <div class="carta">
+          <div class="carta-interior">
+            <div class="carta-frente">
+              <div class="tarjeta-juego">
+                <img src="https://passarelape.vteximg.com.br/arquivos/ids/1381336/Zapatillas-Deportivo-Adidas-Junior-Hp5837-Runfalcon-3_0-K-Textil-Morado---3_5.jpg" alt="" />
+                <div class="juego-descripcion">
+                  <span class="juego-titulo">-- Zapatillas --</span>
+                  <span>S/40,50</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="carta">
+          <div class="carta-interior">
+            <div class="carta-frente">
+              <div class="tarjeta-juego">
+                <img src="https://sydney.pe/wp-content/uploads/2023/09/medias-7-1-1000x1000.jpg" alt="" />
+                <div class="juego-descripcion">
+                  <span class="juego-titulo">-- Medias --</span>
+                  <span>S/10,99</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section></>
   );
 };
 export default CompoPromo;
